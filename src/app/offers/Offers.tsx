04450d161b3946a4527224360a56519e4c859200
broaddcast91 <broaddcast@gmail.com>
2025-01-15 16:49:40 +0530

    update

--- src/app/offers/Offers.tsx
+++ src/app/offers/Offers.tsx
@@ -117,7 +117,7 @@ const Offers = () => {
                         <span className="text-sm font-medium text-gray-900">
                           Save upto
                         </span>{" "}    {/*  ₹* {isNaN(offer.offersAmount) ? "0" : offer.offersAmount.toLocaleString()} */} 
-                          ₹ {isNaN(offer.offersAmount) ? "0" : offer.offersAmount.toLocaleString("en-IN")} *   {/* ₹* {offer.offersAmount}*/}
+                        ₹ <span className="font-medium">{isNaN(offer.offersAmount) ? "0" : offer.offersAmount.toLocaleString("en-IN")}</span><span className="font-bold">*</span>  {/* ₹* {offer.offersAmount}*/}
                       </p>
                       <p className="text-sm mb-2 ">
                         {offer.offersAdditionalDec}
