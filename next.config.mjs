5a753db80e11cfcf64b93977e15e2ea46e9f91a7
broaddcast91 <broaddcast@gmail.com>
2025-10-08 16:27:14 +0530

    updated offers prices

--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,14 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/(.*)",
+        source: '/(.*)',
         headers: [
           {
-            key: "Strict-Transport-Security",
-            value: "max-age=63072000; includeSubDomains; preload",
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload',
           },
           {
-            key: "Content-Security-Policy",
+            key: 'Content-Security-Policy',
             value: `
               default-src 'self';
               img-src 'self' https: data:;
@@ -19,23 +19,23 @@ const nextConfig = {
               font-src 'self' https: data:;
               frame-src 'self' https://www.google.com https://maps.google.com;
               connect-src 'self' https://maps.googleapis.com https://www.google-analytics.com;
-            `.replace(/\n/g, " "),
+            `.replace(/\n/g, ' '),
           },
           {
-            key: "X-Frame-Options",
-            value: "ALLOW-FROM https://www.google.com",
+            key: 'X-Frame-Options',
+            value: 'ALLOW-FROM https://www.google.com',
           },
           {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
           },
           {
-            key: "Referrer-Policy",
-            value: "strict-origin-when-cross-origin",
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
           },
           {
-            key: "Permissions-Policy",
-            value: "geolocation=(self), microphone=(), camera=()",
+            key: 'Permissions-Policy',
+            value: 'geolocation=(self), microphone=(), camera=()',
           },
         ],
       },
