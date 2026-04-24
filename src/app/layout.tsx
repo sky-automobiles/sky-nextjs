0a2ce9e882ba44beb8b8ce65962ef56b743c7961
rahulweb-dev <rahulwebdeveloper12@gmail.com>
2026-04-24 15:52:02 +0530

    updated clarity script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,18 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <head>
+       <Script
+          id="clarity-script"
+          strategy="afterInteractive"
+        >
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "wgnhl2esfz");
+          `}
+        </Script>
         <Script
           src='https://www.googletagmanager.com/gtag/js?id=AW-17597052359'
           strategy='afterInteractive'
