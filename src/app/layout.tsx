fe44f28932269353f76a4a1a4e9703d0dff9cda9
rahulweb-dev <rahulwebdeveloper12@gmail.com>
2026-05-18 14:50:00 +0530

    hided tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,14 +48,14 @@ export default function RootLayout({
         </Script>
 
         {/* ✅ Google Tag Manager */}
-        <Script id="gtm" strategy="afterInteractive">
+        {/* <Script id="gtm" strategy="afterInteractive">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;
             j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
             f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','GTM-5L2MX9GG');`}
-        </Script>
+        </Script> */}
 
         {/* ✅ Facebook Pixel (FIXED) */}
         <Script id="facebook-pixel" strategy="afterInteractive">
