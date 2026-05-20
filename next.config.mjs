1897248f5381fa5ce08c8ddb21ce0a84fae46943
rahulweb-dev <rahulwebdeveloper12@gmail.com>
2026-05-20 11:20:47 +0530

    removed tags

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'www.broaddcast.com/',
+        hostname: 'www.broaddcast.com',
       },
     ],
   },
